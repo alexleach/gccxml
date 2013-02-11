cf02f7d95edcf781a6805120f544246026d76bdd
Alex Leach <beamesleach@gmail.com>
2013-02-11 21:56:24 +0000

    Windows compatibility fix for 'date' command in today.cmake. Fix duplicate objects in libiberty

--- GCC/config_cmake/today.cmake
+++ GCC/config_cmake/today.cmake
@@ -2,16 +2,17 @@
 #  http://www.cmake.org/pipermail/cmake/2009-February/027014.html
 
 MACRO (TODAY RESULT)
-    IF (WIN32)
-        EXECUTE_PROCESS(COMMAND "date" "/T" OUTPUT_VARIABLE ${RESULT})
-        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\3\\2\\1"
-                    ${RESULT} ${${RESULT}})
-    ELSEIF(UNIX)
-        EXECUTE_PROCESS(COMMAND "date" "+%d/%m/%Y" OUTPUT_VARIABLE ${RESULT})
-        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\3\\2\\1"
-                    ${RESULT} ${${RESULT}})
-    ELSE (WIN32)
-        MESSAGE(SEND_ERROR "date not implemented")
-        SET(${RESULT} 000000)
-    ENDIF (WIN32)
+  IF (CYGWIN)
+    EXECUTE_PROCESS(COMMAND "date" "/T" OUTPUT_VARIABLE ${RESULT})
+    string(REGEX REPLACE "(..)/(..)/(....).*" "\\3\\2\\1"
+           ${RESULT} ${${RESULT}})
+  ELSEIF(UNIX)
+    EXECUTE_PROCESS(COMMAND "date" "+%d/%m/%Y" OUTPUT_VARIABLE ${RESULT})
+    string(REGEX REPLACE "(..)/(..)/(....).*" "\\3\\2\\1"
+          ${RESULT} ${${RESULT}})
+  ELSE (CYGWIN)
+    MESSAGE(SEND_ERROR "date not implemented")
+    SET(${RESULT} 000000)
+  ENDIF (CYGWIN)
 ENDMACRO (TODAY)
+
